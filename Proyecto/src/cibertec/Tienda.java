4c95ccc08609d7db0e6703795786c2b84e03269b
elizabethcubas <elizabeth_cubas_16@hotmail.com>
2019-06-30 15:33:29 -0500

    Commit prueba

--- Proyecto/src/cibertec/Tienda.java
+++ Proyecto/src/cibertec/Tienda.java
@@ -1,6 +1,7 @@
 package cibertec;
 
 import java.awt.EventQueue;
+import java.awt.Image;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -12,6 +13,14 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+import java.awt.Toolkit;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import java.awt.SystemColor;
 
 public class Tienda extends JFrame implements ActionListener {
 
@@ -97,6 +106,10 @@ public class Tienda extends JFrame implements ActionListener {
 	// Cantidad de clientes atendidos
 	public static int numeroCliente = 0;
 	
+	//  Usuario y contraseña
+	private static String usuario1 = "admin", contraseña1 = "12345";
+	private static String usuario2 = "vendedor", contraseña2 = "123456";
+	
 	private JMenuBar menuBar;
 	private JMenu mnArchivo;
 	private JMenu mnMantenimiento;
@@ -114,6 +127,14 @@ public class Tienda extends JFrame implements ActionListener {
 	private JMenuItem mntmConfigurarCantidadOptima;
 	private JMenuItem mntmConfigurarPremioSorpresa;
 	private JMenuItem mntmAcercaDeTienda;
+	private JLabel lblContraseña;
+	private JTextField txtUsuario;
+	private JButton btnProcesar;
+	private JButton btnBorrar;
+	private JPasswordField pswContraseña;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel;
+	private JLabel label;
 
 	/**
 	 * Launch the application.
@@ -142,14 +163,16 @@ public class Tienda extends JFrame implements ActionListener {
 	 * Create the frame.
 	 */
 	public Tienda() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Admin\\Downloads\\dream.png"));
 		setTitle(NOMBRE_TIENDA);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 762, 465);
+		setBounds(100, 100, 750, 450);
 		
 		menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
 		mnArchivo = new JMenu("Archivo");
+		mnArchivo.setEnabled(false);
 		menuBar.add(mnArchivo);
 		
 		mntmSalir = new JMenuItem("Salir");
@@ -157,6 +180,7 @@ public class Tienda extends JFrame implements ActionListener {
 		mnArchivo.add(mntmSalir);
 		
 		mnMantenimiento = new JMenu("Mantenimiento");
+		mnMantenimiento.setEnabled(false);
 		menuBar.add(mnMantenimiento);
 		
 		mntmConsultarColchon = new JMenuItem("Consultar colch\u00F3n");
@@ -172,6 +196,7 @@ public class Tienda extends JFrame implements ActionListener {
 		mnMantenimiento.add(mntmListarColchones);
 		
 		mnVentas = new JMenu("Ventas");
+		mnVentas.setEnabled(false);
 		menuBar.add(mnVentas);
 		
 		mntmVender = new JMenuItem("Vender");
@@ -183,6 +208,7 @@ public class Tienda extends JFrame implements ActionListener {
 		mnVentas.add(mntmGenerarReportes);
 		
 		mnConfiguracion = new JMenu("Configuraci\u00F3n");
+		mnConfiguracion.setEnabled(false);
 		menuBar.add(mnConfiguracion);
 		
 		mntmConfigurarDescuentos = new JMenuItem("Configurar descuentos");
@@ -202,6 +228,7 @@ public class Tienda extends JFrame implements ActionListener {
 		mnConfiguracion.add(mntmConfigurarPremioSorpresa);
 		
 		mnAyuda = new JMenu("Ayuda");
+		mnAyuda.setEnabled(false);
 		menuBar.add(mnAyuda);
 		
 		mntmAcercaDeTienda = new JMenuItem("Acerca de Tienda");
@@ -209,12 +236,63 @@ public class Tienda extends JFrame implements ActionListener {
 		mnAyuda.add(mntmAcercaDeTienda);
 		
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		lblContraseña = new JLabel("Contrase\u00F1a");
+		lblContraseña.setBounds(185, 115, 90, 16);
+		contentPane.add(lblContraseña);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setBounds(273, 80, 133, 22);
+		contentPane.add(txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		btnProcesar = new JButton("Procesar");
+		btnProcesar.addActionListener(this);
+		btnProcesar.setBounds(418, 62, 97, 25);
+		contentPane.add(btnProcesar);
+		
+		btnBorrar = new JButton("Borrar");
+		btnBorrar.addActionListener(this);
+		btnBorrar.setBounds(418, 91, 97, 25);
+		contentPane.add(btnBorrar);
+		
+		pswContraseña = new JPasswordField();
+		pswContraseña.setBounds(273, 112, 133, 22);
+		contentPane.add(pswContraseña);
+				
+		//imagen
+		Image iconoImagen = new ImageIcon("C:\\Users\\Admin\\Downloads\\user.png").getImage();
+		ImageIcon icono = new ImageIcon(iconoImagen.getScaledInstance(50, 50, Image.SCALE_SMOOTH));
+		
+		label = new JLabel("");
+		label.setIcon(icono);
+		label.setBounds(185, 56, 66, 55);
+		contentPane.add(label);
+		
+		lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Admin\\Downloads\\Music\\fondo-blanco.jpg"));
+		lblNewLabel.setBounds(131, 34, 460, 121);
+		contentPane.add(lblNewLabel);
+		
+		lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\Admin\\Pictures\\fondo-celeste-trans.png"));
+		lblNewLabel_1.setBounds(0, 0, 732, 377);
+		contentPane.add(lblNewLabel_1);
+		
+
 	}
 
 	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnBorrar) {
+			actionPerformedBtnBorrar(e);
+		}
+		if (e.getSource() == btnProcesar) {
+			actionPerformedBtnProcesar(e);
+		}
 		if ( e.getSource() == mntmListarColchones ) {
 			actionPerformedMntmListarColchones(e);
 		}
@@ -295,4 +373,68 @@ public class Tienda extends JFrame implements ActionListener {
 		DialogConfigurarCantidadOptimaColchones viewConfigurarCantidadOptima = new DialogConfigurarCantidadOptimaColchones();
 		viewConfigurarCantidadOptima.setVisible(true);
 	}
+	protected void actionPerformedBtnProcesar(ActionEvent e) {
+		
+		String xUsuario = leerUsuario();
+		if (xUsuario.equals(usuario1)) {
+			String xContraseña = leerContrasenia();
+			if (xContraseña.equals(contraseña1))
+				mensaje("BIENVENIDO ADMINISTRADOR");
+			else {
+				mensaje1("CONTRASEÑA incorrecta");
+				pswContraseña.setText("");
+				pswContraseña.requestFocus();
+			}
+		}
+		else if (xUsuario.equals(usuario2)) {
+			String xContraseña = leerContrasenia();
+			if (xContraseña.equals(contraseña2))
+				mensaje("BIENVENIDO VENDEDOR");
+			else {
+				mensaje1("CONTRASEÑA incorrecta");
+				pswContraseña.setText("");
+				pswContraseña.requestFocus();
+			}
+		}
+		else if ( xUsuario.isEmpty() ) {
+			mensaje1("Los campos están vacíos");
+			txtUsuario.setText("");
+			txtUsuario.requestFocus();
+		}
+
+		else {
+			mensaje1("USUARIO incorrecto");
+			txtUsuario.setText("");
+			pswContraseña.setText("");
+			txtUsuario.requestFocus();
+		}
+	}
+	
+	//  Métodos tipo void (con parámetros)
+		void mensaje(String s) {
+			JOptionPane.showMessageDialog(this, s);
+			mnArchivo.setEnabled(true);
+			mnMantenimiento.setEnabled(true);
+			mnVentas.setEnabled(true);
+			mnConfiguracion.setEnabled(true);
+			mnAyuda.setEnabled(true);
+		}	
+		
+		void mensaje1(String s) {
+			JOptionPane.showMessageDialog(this, s);
+		}	
+
+	//  Métodos que retornan valor (sin parámetros)
+		String leerUsuario() {
+			return txtUsuario.getText().trim();
+		}
+		String leerContrasenia() {
+			return new String(pswContraseña.getPassword());	
+		}
+	
+		protected void actionPerformedBtnBorrar(ActionEvent e) {
+		txtUsuario.setText("");
+		pswContraseña.setText("");
+		txtUsuario.requestFocus();	
+		}
 }
